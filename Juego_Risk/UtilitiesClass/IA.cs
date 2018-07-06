56e661d19de6844aa9c70dbd098fe69ea098b124
Bryan Macario <bryan.macario12@gmail.com>
2018-07-05 21:03:02 -0600

    New Classes Added
    - Singleton for single board
    - Class IA

---
+++ Juego_Risk/UtilitiesClass/IA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Risk.UtilitiesClass
+{
+    public class IA
+    {
+        /* Empty Builder */
+        public IA()
+        {
+
+        }
+
+        /* Builder */
+
+
+    }
+}
---
+++ Juego_Risk/UtilitiesClass/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Risk.UtilitiesClass
+{
+    public class Singleton
+    {
+        /* Single Instance */
+        private static Singleton _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+
+        /* Add the class map with all countries */
+    }
+}
