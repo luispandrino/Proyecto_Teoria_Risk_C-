a4f4747aad317cf7541f857f9e51cac21a6cd7b0
Unknown <landrinomenendez@gmail.com>
2018-07-05 23:59:48 -0600

    I'm human

---
+++ Juego_Risk/UtilitiesClass/Mapa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Risk.UtilitiesClass
+{
+    class Mapa
+    {
+        Pais Aux;
+        List<Pais> Lista_Paises = new List<Pais>();
+        
+        public void Asignar_Paises()
+        {
+            Lista_Paises.Add(Aux = new Pais("AFganistan", 0, 3, 1, 0, 0, 0));
+            Lista_Paises.Add(Aux = new Pais("", 0, 3, 1, 0, 0, 0));
+
+        }
+       
+      
+    }
+    
+}
---
+++ Juego_Risk/UtilitiesClass/Pais.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Risk.UtilitiesClass
+{
+    class Pais
+    {
+        private string Nombre;
+        private int Tropas;
+        private int Pertenencia;
+        private int Imp;
+        private int P_Asig;
+        private int P_ATK;
+        private int P_Fort;
+        public Pais (string Nombre_Pais, int Tropas, int Pertenece_a, int Importancia, int Prioridad_Asig, int Prioridad_ATK, int Prioridad_Fort)
+        {
+            this.Nombre = Nombre_Pais;
+            this.Tropas = Tropas;
+            this.Pertenencia = Pertenece_a;
+            this.Imp = Importancia;
+            this.P_Asig = Prioridad_Asig;
+            this.P_ATK = Prioridad_ATK;
+            this.P_Fort = Prioridad_Fort;
+        }
+    }
+}
