fd1c3b795827c5bf6042743ed6aa18d643cf6953
Unknown <landrinomenendez@gmail.com>
2018-07-06 11:13:41 -0600

    Who knows...

--- Juego_Risk/Form1.Designer.cs
+++ Juego_Risk/Form1.Designer.cs
@@ -28,7 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button19 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_Groenlandia = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.button19.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button19.Location = new System.Drawing.Point(447, 121);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(34, 26);
+            this.button19.TabIndex = 39;
+            this.button19.Text = "0";
+            this.button19.UseVisualStyleBackColor = false;
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.Olive;
+            this.button18.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button18.Location = new System.Drawing.Point(490, 417);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(34, 26);
+            this.button18.TabIndex = 38;
+            this.button18.Text = "0";
+            this.button18.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.Olive;
+            this.button17.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button17.Location = new System.Drawing.Point(400, 417);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(34, 26);
+            this.button17.TabIndex = 37;
+            this.button17.Text = "0";
+            this.button17.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Olive;
+            this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button16.Location = new System.Drawing.Point(426, 324);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(34, 26);
+            this.button16.TabIndex = 36;
+            this.button16.Text = "0";
+            this.button16.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Olive;
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button15.Location = new System.Drawing.Point(400, 270);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(34, 26);
+            this.button15.TabIndex = 35;
+            this.button15.Text = "0";
+            this.button15.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Olive;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button14.Location = new System.Drawing.Point(400, 356);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(34, 26);
+            this.button14.TabIndex = 34;
+            this.button14.Text = "0";
+            this.button14.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Olive;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button13.Location = new System.Drawing.Point(337, 296);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(34, 26);
+            this.button13.TabIndex = 33;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Chocolate;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button12.Location = new System.Drawing.Point(166, 270);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(34, 26);
+            this.button12.TabIndex = 32;
+            this.button12.Text = "0";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Chocolate;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Location = new System.Drawing.Point(182, 334);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(34, 23);
+            this.button11.TabIndex = 31;
+            this.button11.Text = "0";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Chocolate;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Location = new System.Drawing.Point(183, 384);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(34, 26);
+            this.button10.TabIndex = 30;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Chocolate;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Location = new System.Drawing.Point(222, 310);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(34, 26);
+            this.button9.TabIndex = 29;
+            this.button9.Text = "0";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.GreenYellow;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Location = new System.Drawing.Point(122, 238);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(34, 26);
+            this.button8.TabIndex = 28;
+            this.button8.Text = "0";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.GreenYellow;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(205, 121);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 26);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "0";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.GreenYellow;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Location = new System.Drawing.Point(155, 121);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(31, 25);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "0";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.GreenYellow;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(48, 78);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(28, 24);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "0";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.GreenYellow;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(166, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 24);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "0";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.GreenYellow;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(103, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 26);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(103, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 26);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "0";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(103, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 26);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Groenlandia
+            // 
+            this.Btn_Groenlandia.BackColor = System.Drawing.Color.GreenYellow;
+            this.Btn_Groenlandia.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Groenlandia.Location = new System.Drawing.Point(252, 50);
+            this.Btn_Groenlandia.Name = "Btn_Groenlandia";
+            this.Btn_Groenlandia.Size = new System.Drawing.Size(34, 26);
+            this.Btn_Groenlandia.TabIndex = 20;
+            this.Btn_Groenlandia.Text = "0";
+            this.Btn_Groenlandia.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -36,7 +276,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Juego_Risk.Properties.Resources.Mapa;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(690, 451);
+            this.ClientSize = new System.Drawing.Size(773, 514);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Groenlandia);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -44,6 +304,27 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_Groenlandia;
     }
 }
 
