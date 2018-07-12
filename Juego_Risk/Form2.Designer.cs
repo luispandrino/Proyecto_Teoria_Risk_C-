1828830113a742f175c6168cb546ece55b70241a
Luis Andrino <landrinomenendez@gmail.com>
2018-07-12 08:01:51 -0600

    At times like this I wish I was a Garbage Man.

--- Juego_Risk/Form2.Designer.cs
+++ Juego_Risk/Form2.Designer.cs
@@ -93,6 +93,7 @@
             this.Controls.Add(this.txtNombre);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Risk Simulator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
